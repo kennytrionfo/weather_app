0afa6cd7d23d5900aec6b13b50517927b82073b8
Kenny T <ktrionfo@integracore.com>
2015-12-28 20:12:34 +0000

    adding routes and controllers

--- Starter/app.js
+++ Starter/app.js
@@ -1,5 +1,25 @@
-/////////////////////////========MODULES=======\\\\\\\\\\\\\\\\\\\\\\\\\\\
-var weatherApp = angular.module('weatherApp', []);
+/////////////////////////========MODULES, with any services as dependencies=======\\\\\\\\\\\\\\\\\\\\\\\\\\\
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
+/////////////////////////========CONTROLLERS=======\\\\\\\\\\\\\\\\\\\\\\\\\\\
+weatherApp.controller('homeController', ['$scope', function($scope){
 
+}]);
+
+weatherApp.controller('forcastController', ['$scope', function($scope){
+
+}]);
+
+/////////////////////////========ROUTES=======\\\\\\\\\\\\\\\\\\\\\\\\\\\
+weatherApp.config(function ($routeProvider){
+	$routeProvider
+		.when('/', {
+			templateUrl: 'pages/home.htm',
+			controller: 'homeController'
+		})
+		.when('/', {
+			templateUrl: 'pages/forcast.htm',
+			controller: 'forcastController'
+		})
+})
 
