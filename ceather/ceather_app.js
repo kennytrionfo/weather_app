7351a17fb1b09b1df2cb31f6428bad8ca24d1542
Kenny T <ktrionfo@integracore.com>
2016-01-13 00:00:10 +0000

    added a custom directive in a folder

--- ceather/ceather_app.js
+++ ceather/ceather_app.js
@@ -9,8 +9,27 @@ app.controller('homeController', ['$scope', 'cityService', function($scope, city
 		cityService.city = $scope.city;
 	})
 }]);
-app.controller('forecastController',['$scope', 'cityService',  function($scope, cityService){
+app.controller('forecastController',['$scope', '$resource', '$routeParams', 'cityService',  function($scope, $resource, $routeParams, cityService){
 	$scope.city = cityService.city;
+	$scope.days = $routeParams.days || '1';
+	$scope.weatherApi = 
+		$resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=d8f5102c89d08caf442ba64a6bcda871", 
+			{ 
+				callback: "JSON_CALLBACK"
+			},
+			{
+				get: {method: "JSONP"}
+			}
+		);
+	$scope.weatherResult = $scope.weatherApi.get(
+			{
+				q: $scope.city,
+				cnt: $scope.days
+			}
+		);
+
+	// console.log($scope.weatherResult );
+
 }]);
 
 /////////////////////////========ROUTES=======\\\\\\\\\\\\\\\\\\\\\\\\\\\
@@ -24,9 +43,22 @@ app.config(function ($routeProvider){
 			templateUrl: 'pages/forecast.html',
 			controller: 'forecastController'
 		})
+		.when('/forecast/:days', {
+			templateUrl: 'pages/forecast.html',
+			controller: 'forecastController'
+		});
 })
 
 /////////////////////////========SERVICES=======\\\\\\\\\\\\\\\\\\\\\\\\\\\
 app.service('cityService', function(){
 	this.city = "Sebastian, FL";
+});
+
+/////////////////////////========DiRECTiVES=======\\\\\\\\\\\\\\\\\\\\\\\\\\\
+app.directive('weatherResult', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'directives/weatherReport.html',
+		replace: true,
+	}
 });
