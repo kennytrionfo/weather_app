487257f2699d8d2e463e44af7992020194a2f860
Kenny T <ktrionfo@integracore.com>
2016-01-19 19:01:35 +0000

    updated angular-route path

--- heather/heather_app.js
+++ heather/heather_app.js
@@ -1,8 +1,25 @@
-var app = angular.module('heatherApp', ['ngroute', 'ngResource']);
+var app = angular.module('heatherApp', ['ngRoute', 'ngResource'] );
 
 /////////////////////////========CONTROLLERS=======\\\\\\\\\\\\\\\\\\\\\\\\\\\
 app.controller('homeController', ['$scope', function($scope){
 
 }]);
 
+app.controller('forecastController', ['$scope', function($scope){
+
+}]);
+
+/////////////////////////========ROUTES=======\\\\\\\\\\\\\\\\\\\\\\\\\\\
+app.config(function ($routeProvider){
+	$routeProvider
+		.when('/', {
+			templateUrl: 'pages/home.html',
+			controller: 'homeController'
+		})
+
+		.when('/forecast', {
+			templateUrl: 'pages/forecast.html',
+			controller: 'forecastController'
+		})
+});
 
